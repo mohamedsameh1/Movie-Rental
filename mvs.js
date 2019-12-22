f170c16b6c182c754150352aa983d5e77f81a901
mohamedsameh1 <58793458+mohamedsameh1@users.noreply.github.com>
2019-12-23 00:53:42 +0200

    Add files via upload

--- mvs.js
+++ mvs.js
@@ -1,13 +1,39 @@
-var x = 3;
+var x = 100	;
 function cps() {
     x = parseInt(x);
     x--;
     document.getElementById('cpscnt').innerHTML = "Availabe Copies ="+x;
     if(x<0){
         window.alert("there is  no copies available");
-        x=1;
     }
     }
-function search(){
-    
-}
+	function fightclub()
+	{
+		var movie="fight club";
+		var price=30;
+		var rent=document.getElementById("fightc").value;
+	}
+	function legend()
+	{
+		var movie="legend";
+		var price=35;
+		var rent=document.getElementById("legend").value;
+	}
+	function inception()
+	{
+		var movie="inception";
+		var price=28;
+		var rent=document.getElementById("inc").value;
+	}
+	function lac()
+	{
+		var movie="lac";
+		var price=15;
+		var rent=document.getElementById("lac").value;
+	}
+	function godfather()
+	{
+		var movie="godfather";
+		var price=50;
+		var rent=document.getElementById("gff").value;
+	}
