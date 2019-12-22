b1546a5c1c0dccbfcf2647970121bc1871960dc2
Alaanosser1 <58793500+Alaanosser1@users.noreply.github.com>
2019-12-22 21:50:27 +0200

    Add files via upload

--- mvs.js
+++ mvs.js
@@ -1,29 +1,13 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+var x = 3;
+function cps() {
+    x = parseInt(x);
+    x--;
+    document.getElementById('cpscnt').innerHTML = "Availabe Copies ="+x;
+    if(x<0){
+        window.alert("there is  no copies available");
+        x=1;
+    }
+    }
+function search(){
+    
 }
-
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) 
-  {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) 
-  {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
